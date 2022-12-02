0db23c2e70117126844e35aa1907101f4a1b06b0
karimliaghabala <agabala.93@gmail.com>
2022-12-03 03:30:13 +0400

    feedback update

--- js/script.js
+++ js/script.js
@@ -4,16 +4,13 @@ const form = document.getElementById('formFeedback');
 
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
-    submit.value = 'Göndərilir';
     const serviceID = 'default_service';
     const templateID = 'template_trcpd9k';
-
+ 
     emailjs.sendForm(serviceID, templateID, form)
      .then(() => {
-        submit.value = 'Send Email';
-       alert('Mektub gonderildi!');
+       alert('Məktub Göndərildi');
      }, (err) => {
-        submit.value = 'Send Email';
        alert(JSON.stringify(err));
      });
 })
