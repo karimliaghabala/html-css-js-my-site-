ab15decf9e8562c49220f96ea1301a6992b7064a
karimliaghabala <agabala.93@gmail.com>
2022-12-03 01:31:10 +0400

    update html

--- js/script.js
+++ js/script.js
@@ -8,19 +8,3 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     alert("Mektub gonderildi")
 })
-// function sendEmail(){
-//     Email.send({
-//         Host: "smtp.gmail.com",
-//         Username: "agabalainfo93@gmail.com",
-//         Password: "0508357211Aga",
-//         To: 'agabalainfo93@gmail.com',
-//         From: document.getElementById('exampleFormControlInput3').value,
-//         Subject: document.getElementById('exampleFormControlInput2').value,
-//         Body: "Name: " + document.getElementById('exampleFormControlInput1').value + 
-//         "<br> Email: " + document.getElementById('exampleFormControlInput3').value + 
-//         "<br> Textin mətni: " + document.getElementById('exampleFormControlTextarea1').value
-
-//     }).then(
-//         message => alert("Məktub göndərildi")
-//     )
-// }
